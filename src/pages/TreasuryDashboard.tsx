b9be593ad8b7830ef6a9b790989d206413a3f505
[dyad] <git@dyad.sh>
2025-09-17 13:14:51 -0300

    [dyad] Ajustei os banners de resumo no painel da tesouraria para ocuparem o espaço da tela de forma mais responsiva, centralizando as classes de grade no componente `TreasurySummary` e removendo redundâncias. - wrote 2 file(s)

--- src/pages/TreasuryDashboard.tsx
+++ src/pages/TreasuryDashboard.tsx
@@ -192,13 +192,11 @@ const TreasuryDashboard = () => {
         Painel da Tesouraria
       </h1>
 
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-        <TreasurySummary
-          totalIncome={totalIncome}
-          totalExpenses={totalExpenses}
-          balance={balance}
-        />
-      </div>
+      <TreasurySummary
+        totalIncome={totalIncome}
+        totalExpenses={totalExpenses}
+        balance={balance}
+      />
 
       <Card>
         <CardHeader>
